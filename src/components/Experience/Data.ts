9ddc009495ac40b9618aa3ce96ff17adde4a2ae4
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-09-02 12:36:18 +0100

    feat: updated experience

--- src/components/Experience/Data.ts
+++ src/components/Experience/Data.ts
@@ -2,31 +2,31 @@ export const Experience = [
   {
     name: "Frontend developer",
     location: "remote",
-    date: "August 2022- November 2022",
+    date: "Aug 2022- Feb 2023",
     company: "Qataloog"
   },
   {
     name: "Frontend developer",
     location: "Lagos, Nigeria",
-    date: "March 2022 - Present",
+    date: "Mar 2022 - Nov 2022",
     company: "Konveyor",
   },
   {
     name: "Frontend Developer Intern",
     location: "Lagos, Nigeria",
-    date: "June 2021 - September 2021",
+    date: "Jun 2021 - Sept 2021",
     company: "Minority Programmers Association",
   },
   {
     name: "Frontend Engineer",
     location: "Lagos, Nigeria",
-    date: "September, 2021",
+    date: "Sept, 2021",
     company: "Enye Hackathon",
   },
   {
     name: "Frontend Developer Intern",
     location: "Lagos, Nigeria",
-    date: "June 2020 - September 2020",
+    date: "Jun 2020 - Sept 2020",
     company: "HNG.Tech",
   },
 ];
