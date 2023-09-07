fc0039ed905350efde3aed68a5c9e9e8cce56799
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-09-07 12:57:36 +0100

    fix: fixed tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
-
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-    theme: {
-    extend: {},
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
   },
-plugins: [
-  plugin(function ({ addVariant, e, postcss }) {
-    addVariant('firefox', ({ container, separator }) => {
-      const isFirefoxRule = postcss.atRule({
-        name: '-moz-document',
-        params: 'url-prefix()'
-      })
-      isFirefoxRule.append(container.nodes)
-      container.append(isFirefoxRule)
-      isFirefoxRule.walkRules(rule => {
-        rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`
-      })
-    })
-  })
-]
+  plugins: []
 }
-
