1d277bc2692f05532b1a28d76b8364978ef7208b
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2025-12-01 20:55:01 +0100

    style: refine HeroSection CSS for improved layout and responsiveness; clean up Navbar imports

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ThemeContext } from "../../provider/theme.provider";
 import { Link } from "react-scroll";
